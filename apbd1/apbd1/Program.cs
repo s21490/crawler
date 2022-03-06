c2f11c4fc542e49cb788280531f8ab5c58a2c6f2
Tomasz Wierzbowski <s21490@pjwstk.edu.pl>
2022-03-06 12:38:00 +0100

    initial commit"

---
+++ apbd1/apbd1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace apbd1
+{
+    class Program
+    {
+        public async static Task Main(string[] args)
+        {
+            var websiteUrl = args[0];
+            // Console.WriteLine(websiteUrl);
+
+            var httpClient = new HttpClient();
+            //var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync(websiteUrl);
+            //Console.WriteLine(response);
+
+            var content = await response.Content.ReadAsStringAsync();
+            //Console.WriteLine(content);
+
+            var regex = new Regex(@" .*@pja.* ");
+
+            var matchCollection = regex.Matches(content);
+
+            foreach (Match item in matchCollection) {
+                Console.WriteLine(item.Value);
+            }
+        }
+    }
+}
