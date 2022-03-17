78ead861fbe1957662ce2a792a39c7d4adf1fe53
Tomasz Wierzbowski <tomaswierzbowski@gmail.com>
2022-03-17 12:40:58 +0100

    homework

--- apbd1/apbd1/Program.cs
+++ apbd1/apbd1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -7,26 +9,62 @@ namespace apbd1
 {
     class Program
     {
+        public static bool isValidUrl = false;
         public async static Task Main(string[] args)
         {
+            //validations
+            if (args.Length == 0) throw new ArgumentNullException();
             var websiteUrl = args[0];
-            // Console.WriteLine(websiteUrl);
+            CheckIfValidUrl(websiteUrl);
+
+            if (isValidUrl == false) throw new ArgumentException();
 
             var httpClient = new HttpClient();
-            //var httpClient = new HttpClient();
 
             var response = await httpClient.GetAsync(websiteUrl);
-            //Console.WriteLine(response);
 
-            var content = await response.Content.ReadAsStringAsync();
-            //Console.WriteLine(content);
+            try
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var regex = new Regex(@"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+");
 
-            var regex = new Regex(@"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+");
+                var matchCollectionString = regex.Matches(content).Cast<Match>()
+                .Select(m => m.Value)
+                .ToArray();
 
-            var matchCollection = regex.Matches(content);
+                string[] matchCollectionStringUnique = matchCollectionString.Distinct().ToArray();
 
-            foreach (Match item in matchCollection) {
-                Console.WriteLine(item.Value);
+                if (matchCollectionStringUnique.Length > 0)
+                {
+                    foreach (string item in matchCollectionStringUnique)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+                else {
+                    Console.WriteLine("Nie znaleziono adresów email");
+                }
+            }
+            catch(HttpRequestException) {
+                Console.WriteLine("Błąd w czasie pobierania strony");
+            }
+
+            // freeing http client resources
+            httpClient.Dispose();
+            response.Dispose();
+        }
+        static void CheckIfValidUrl(string url) {
+            try
+            {
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+                request.Method = "HEAD";
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                response.Close();
+                isValidUrl = true;
+            }
+            catch
+            {
+                isValidUrl =  false;
             }
         }
     }
